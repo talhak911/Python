5b9e9f62675d364f33493fac02c5d0b60d83da72
talhak911 <talhakhan8300@gmail.com>
2023-10-18 20:06:16 +0500

    f string

--- 02class/class2.py
+++ 02class/class2.py
@@ -9,7 +9,7 @@ Name :
 Age :
 """
 
-# F-string - directly convert to string-any expression inside {} - latest version
+# F-string - directly convert to string-any expression can be inside {} - latest version
 new_card : str = f""" 
 Card
 Name : {name}
@@ -25,6 +25,18 @@ print(" a is {} and b is {}".format(first,second))
 info : str = """a is {b} and b is {a}
 """.format(a=first,b=second)
 print(info)
+
+
+################# second index #### first
+info1 : str = """a is {1} and b is {0}
+""".format(2,3)  #### (first,second...)
+print(info1)
+
+
+info2 : str = """a is {} and b is {}
+""".format(2,3)
+print(info2)
+
 
 ###################
 
