e6c2c35fc5273891d819887beea384c360140be8
talhak911 <talhakhan8300@gmail.com>
2023-10-18 20:05:45 +0500

    class 1

--- 01class/class1.py
+++ 01class/class1.py
@@ -1,10 +1,12 @@
 print("Hello word")
 #dynamic variable
 name="Talha"
-print(name)
+print("My name",name)
 
 #static variable
 name1 : str ="Talha Khan"
 print(name1)
 
+mytuple :tuple[str,int]=("talha",20)
+print(mytuple)
 # to check error on command mypy class1.py
