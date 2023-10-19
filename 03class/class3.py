16c615be6c2885878fc93db8e89295e19185364e
talhak911 <talhakhan8300@gmail.com>
2023-10-19 21:07:18 +0500

    class3

---
+++ 03class/class3.py
@@ -0,0 +1,14 @@
+a : int = 7
+b : int = 2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+print(f"mod of {a} and 2 is",a % 2)
+print(f"{a+b}")
+
+print(f"square of {a} is",a**2)
+
+print(14.8/3)
+print("Float divisoin ",14.8//3) 
