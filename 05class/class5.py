16feb094561fa465bea2a40f8f89047b03a68c4b
Muhammad Talha <143874100+talhak911@users.noreply.github.com>
2023-10-22 18:31:19 +0500

    comprehensive for loop and list

--- 05class/class5.py
+++ 05class/class5.py
@@ -2,3 +2,15 @@ names : list[str] = ["Sir Zia","Muhammad qasim", "Dr noman"]
 for name in names:
     print(f"Welcome Dear Teacher {name.title()}")
 print("Team PIAIC")
+
+print("new program")
+numbers = list([i for i in range(1,11)])
+squareEven: list[int]=[]
+for number in numbers:
+     print(f"square of {number} is {number**2}")
+    
+for number in numbers[1::2]:
+    print(f"square of even number {number} is {number**2}")
+    squareEven.append(number**2)
+print("List of even numbers",squareEven)
+
