c84f72eceb612f94e9b39cb5f43a6b3c13e3b132
Muhammad Talha <143874100+talhak911@users.noreply.github.com>
2023-10-27 22:27:36 +0500

    nested for loop

--- 08class/class8.py
+++ 08class/class8.py
@@ -7,3 +7,18 @@ data: dict[key,value] = {"name":"Talha"
                          }
 
 print(data["Hobby"][2])
+
+numbers : list[int]= list(range(1,21))
+print("List of even numbers")
+for n in numbers:
+    
+    if n %2 ==0:
+        print(n)
+print("List of odd numbers")   
+     
+for n in numbers:    
+    if n%2!=0:
+        print(n)
+for n in numbers:
+    for m in range(1,11):
+        print(f"{n} x {m} = {n*m}")
