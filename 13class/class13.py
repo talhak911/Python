3d436ece57daa8af0eba0467a2f81fc61cc7d2cd
Muhammad Talha <143874100+talhak911@users.noreply.github.com>
2023-11-11 22:03:06 +0500

    Update class13.py

--- 13class/class13.py
+++ 13class/class13.py
@@ -3,3 +3,19 @@ b:int = 20
 c = a+b
 c +=c
 print(c)
+import sys 
+ a : int = 1 
+ print("First assignment address of int variable a ",id(a)) 
+ b=a 
+ print(f"b=a ,  value {b} address of b ",id(b)) 
+ b=10 
+ print("b=10 ,  address of b",id(b)) 
+ print(f"value {a} ,  address of a ",id(a)) 
+ print("Start") 
+  
+ print(type(sys.argv))  
+  
+ if sys.argv[1] == '-g': 
+     print("Installing package globally") 
+  
+ print(sys.argv) # iterative data type list[str] 0=filename **value user define
