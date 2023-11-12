66117c8a0393e2588e17c8a9cc77aa3daad2570e
Muhammad Talha <143874100+talhak911@users.noreply.github.com>
2023-11-12 21:07:37 +0500

    Update class5.py

--- 05class/class5.py
+++ 05class/class5.py
@@ -14,3 +14,18 @@ for number in numbers[1::2]:
     squareEven.append(number**2)
 print("List of even numbers",squareEven)
 
+
+a : int = 7 
+ b : int = 2 
+ print(a+b) 
+ print(a-b) 
+ print(a*b) 
+ print(a/b) 
+  
+ print(f"mod of {a} and 2 is",a % 2) 
+ print(f"{a+b}") 
+  
+ print(f"square of {a} is",a**2) 
+  
+ print(14.8/3) 
+ print("Float divisoin ",14.8//3) 
