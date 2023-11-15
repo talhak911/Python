6df3bce6d22c0a5caec213c1311e4844451cc2db
Muhammad Talha <143874100+talhak911@users.noreply.github.com>
2023-11-15 22:30:38 +0500

    Update class2.py

--- 02class/class2.py
+++ 02class/class2.py
@@ -1,3 +1,15 @@
+print("Hello word") 
+ #dynamic variable 
+ name="Talha" 
+ print("My name",name) 
+  
+ #static variable 
+ name1 : str ="Talha Khan" 
+ print(name1) 
+  
+ mytuple :tuple[str,int]=("talha",20) 
+ print(mytuple) 
+ # to check error on command mypy #class1.py
 #space with equal
 name:str = "talha"
 education: str = "Bscs"
